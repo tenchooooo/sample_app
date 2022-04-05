1f78d6ee1f36cc6a2e550f8200446aff0b2c775a
tenchooooo <tenchooo.227@gmail.com>
2022-04-05 02:13:29 +0000

    [finish]3-5

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -18,13 +18,19 @@ class ListsController < ApplicationController
   end
 
   def show
-    @list = List.find([:id])
+    @list = List.find(params[:id])
   end
 
   def edit
     @list = List.find(params[:id])
   end
   
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
+  end
+    
   private
   # ストロングパラメータ
   def list_params
